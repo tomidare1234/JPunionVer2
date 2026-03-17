11d15bf25c5134e739cf4fd20f0976a8f7a6321f
tomidare1234 <tomidare1234@gmail.com>
2026-03-17 15:42:26 +0900

    Add files via upload

---
+++ const.js
@@ -0,0 +1,32 @@
+const colors = [
+    ["リセット", "000000"], ["dark gray", "3c3c3c"], ["gray", "787878"], ["light gray", "d2d2d2"], ["white", "ffffff"],
+    ["deep red", "600018"], ["red", "ed1c24"], ["orange", "ff7f27"], ["gold", "f6aa09"], ["yellow", "f9dd3b"],
+    ["light yellow", "fffabc"], ["dark green", "0eb968"], ["green", "13e67b"], ["light green", "87ff5e"],
+    ["dark teal", "0c816e"], ["teal", "10aea6"], ["light teal", "13e1be"], ["cyan", "60f7f2"],
+    ["dark blue", "28509e"], ["blue", "4093e4"], ["indigo", "6b50f6"], ["light indigo", "99b1fb"],
+    ["dark purple", "780c99"], ["purple", "aa38b9"], ["light purple", "e09ff9"],
+    ["dark pink", "cb007a"], ["pink", "ec1f80"], ["light pink", "f38da9"],
+    ["dark brown", "684634"], ["brown", "95682a"], ["beige", "f8b277"],
+    ["medium gray", "aaaaaa"], ["dark red", "a50e1e"], ["light red", "fa8072"],
+    ["dark orange", "e45c1a"], ["dark goldenrod", "9c8431"], ["goldenrod", "c0a830"],
+    ["light goldenrod", "e8d45f"], ["dark olive", "4a6b3a"], ["olive", "5a944a"], ["light olive", "84c573"],
+    ["dark cyan", "0f799f"], ["light cyan", "bbfaf2"], ["light blue", "7cc5fc"],
+    ["dark indigo", "4d31b8"], ["dark slate blue", "4a4284"], ["slate blue", "7a71c4"], ["light slate blue", "b5aef1"],
+    ["dark peach", "9b5249"], ["peach", "d18078"], ["light peach", "fab6a4"],
+    ["light brown", "dba463"], ["dark tan", "7b6352"], ["tan", "9c846b"], ["light tan", "d6b594"],
+    ["dark beige", "d18051"], ["light beige", "ffc5a5"],
+    ["dark stone", "6d643f"], ["stone", "948c6b"], ["light stone", "cdc59e"],
+    ["dark slate", "333941"], ["slate", "6d758d"], ["light slate", "b3b9d1"]
+];
+const Btargets = [
+    [1, 5, "皇居", 33],
+    [6, 2, "個人で暴れたい会", 24],
+    [8, 8, "沖ノ鳥島", 4],
+    [7, 7, "南鳥島", 5],
+    [8, 1, "竹島", 7],
+    [1, 2, "赤軍", 6],
+    [3, 7, "緑の崇拝者", 12],
+    [4, 0, "個人勢", 2],
+    [6, 5, "suzuki", 26]
+];
+//[x,y,"text","color"]
